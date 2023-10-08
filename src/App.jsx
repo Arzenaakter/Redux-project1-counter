c0111edfe0da894e72358b8c83313c9e1597390c
Arzena Akter <arzenaakter@gmail.com>
2023-10-08 23:47:15 +0600

    use stated and nested component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
+import { useState } from 'react'
 import './App.css'
+import Stats from './components/Stats'
 import Counter from './components/counter'
 
+
+const initialState = [
+  {
+    id: '01',
+    count: 0,
+  },
+  {
+    id:'02',
+    count:0,
+  }
+]
+
 function App() {
+        const [state, setState] = useState(initialState);
+
+        const totalCount = ()=>{
+          return state.reduce((total, currentCounterElement) => total + currentCounterElement.count, 0)
+        }
+
+        const increment = (id)=>{
+          const updatedCounter = state.map( c => {
+            if(c.id === id){
+              return {
+                ...c,
+                count: c.count + 1
+              }
+
+            };
+            return {...c}
+          });
+          setState(updatedCounter)
+            
+           }
+
+      const decrement = (id) =>{
+        const updatedCounter = state.map( c => {
+          if(c.id === id){
+            return {
+              ...c,
+              count: c.count - 1
+            }
+
+          };
+          return {...c}
+        });
+        setState(updatedCounter)
+           
+         }
+
  
 
   return (
@@ -13,7 +63,17 @@ function App() {
             </h1>
 
             <div className="max-w-md mx-auto mt-10 space-y-5">
-             <Counter/>
+              {
+                state.map( counterObj =>
+                  <Counter 
+                  key={counterObj.id}  
+                  id={counterObj.id}  
+                  count={counterObj.count}
+                  incrementHandler = {()=> increment(counterObj.id)}
+                  decrementHandler = {()=> decrement(counterObj.id)}
+                  /> )
+              }
+             <Stats count ={totalCount()}/>
             </div>
         </div>
     </>
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,15 @@
+
+
+
+const Buttons = ({children,handler}) => {
+    return (
+        <button
+        onClick={ handler}
+            className="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment"
+        >
+           {children}
+        </button>
+    );
+};
+
+export default Buttons;
